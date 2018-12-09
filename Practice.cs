22ebf79ffc3f08f80903684d703a42cddf8bab25
Андрей М <librandron@gmail.com>
2018-12-10 01:49:26 +0300

    Block 2 done.

--- Practice.cs
+++ Practice.cs
@@ -41,8 +41,8 @@ namespace ConsoleApp7
 
 
             //1.3 CHAR to DECIMAL
-            decimal charToDecimalImplict = place;       //IMPLICIT: COMPILING
-            decimal charToDecimalExplict = (decimal)place; //EXPLICIT: COMPILING
+            decimal charToDecimalImplict = place;       //IMPLICIT: CONVERT IN 51
+            decimal charToDecimalExplict = (decimal)place; //EXPLICIT: COMPILING BUT CONVERT INTO 0
            // decimal charToDecimalUsingConverter = Convert.ToDecimal(place); //System.InvalidCastException: 'Недопустимое приведение "Char" к "Decimal".'
 
 
@@ -56,12 +56,27 @@ namespace ConsoleApp7
             //2. STRING CONVERSION
 
             //2.1 STRING to CHAR
+            //char stringToCharImplict = name; //IMPLICIT: NOT COMPILING
+            //char stringToCharExplict = (char)name; //EXPLICIT: NOT COMPILING
+            //char stringToCharUsingConverter = Convert.ToChar(name); //System.FormatException: 'Длина строки должна составлять один знак.'
+
 
             //2.2 STRING to BOOL
+            //bool stringToBoolImplict = hasPhoto; //IMPLICIT: NOT COMPILING
+            //bool stringToBoolExplict = (bool)hasPhoto; //EXPLICIT: NOT COMPILING
+            bool stringToBoolUsingCoverter = Convert.ToBoolean(hasPhoto); //CONVERT INTO BOOLEAN VALUE TRUE
 
             //2.3 STRING to DECIMAL
+            //decimal stringToDecimalImplict = flatNumber; //IMPLICIT: NOT COMPILING
+            //decimal stringToDecimalExplict = (decimal)flatNumber; //EXPLICIT: NOT COMPILING
+            decimal stringToDecimalUsingConverter = Convert.ToDecimal(flatNumber); //CONVERT INTO DECIMAL 34
+
 
             //2.4 STRING to INT
+            //int stringToIntImplict = flatNumber; //IMPLICIT: NOT COMPILING
+            //int stringToIntExplict = (int)flatNumber; //EXPLICIT: NOT COMPILING
+            int stringToIntUsingConverter = Convert.ToInt32(flatNumber); //CONVERT INTO INT 34
+
 
             //3. BOOL CONVERSION
 
